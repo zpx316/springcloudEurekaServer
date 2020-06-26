b666798ac3fb74ea3c26b0916901707fd66bd1c6
29138386@qq.com <pengzhang4@huashenghaoche.com>
2020-06-26 14:40:06 +0800

    初始化提交

---
+++ src/main/java/cn/zp/springcloud/EurekaApplication.java
@@ -0,0 +1,20 @@
+package cn.zp.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author zp
+ * @Description:
+ * @date 2020/6/26 下午2:33
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
